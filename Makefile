fca4fc27359c6c9d8e8ae9e8fec954550bfab6c3
Utkarsh Singh <utkarsh@Utkarshs-MacBook-Pro-2.local>
2020-03-29 10:41:29 -0700

    final_commit

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
+default: cacheperf
+
 CXX=g++
 CFLAGS=--std=c++11 -fopenmp
 
-all: cache_perf.cpp
-        $(CXX) $(CFLAGS) cache_perf.cpp -o cacheperf
+cacheperf: cache_perf.cpp
+	$(CXX) $(CFLAGS) cache_perf.cpp -o cacheperf
 
 clean:
-        rm -f *.o cacheperf;
+	rm -f *.o cacheperf;
---
+++ run_all.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+make clean
+make
+
+#Read - Threads - Random
+  for t in 1 2 4 8 12 16
+  do
+        echo -e "Read Random access with $t threads...\n"
+        ./cacheperf -t $t
+  done
+
+
+
+ #Read-Write - Threads - Random
+ for t in 1 2 4 8 12 16
+ do
+        echo -e "Read-Write Random access with $t threads..\n"
+        ./cacheperf -t $t -rw
+ done
+
+ #Read - Threads - Strided
+ for t in 1 2 4 8 12 16 
+ do
+ 	echo -e "Read Random Strided access with $t threads..\n"
+ 	./latency -t $t -s
+ done
+ 
+ 
+ 
+ #Read-Write - Threads - Strided
+ for t in 1 2 4 8 12 16 
+ do
+ 	echo -e "Read-Write Strided access with $t threads..\n"
+ 	./latency -t $t -s -rw
+ done
